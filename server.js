d49b7dbbb540de1703f3b090874cdf3ac0d8af19
XenoXzOFF <xenoxzperso@gmail.com>
2025-12-21 17:21:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -251,5 +251,5 @@ app.post('/admin/delete/:userId', (req, res) => {
     res.redirect('/admin');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Port : ${PORT}`));
